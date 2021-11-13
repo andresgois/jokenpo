78a591dcb20983f85c37f1c8e035a5ef8faf1d52
Andre gois <andre.s.gois3@gmail.com>
2021-11-13 14:06:43 -0300

    creating test component to work with props

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./App.module";
+import { Teste } from "./components/Teste/teste";
 
 const App = () => {
 
   return(
     <View>
-      <Text>Hello wolrd</Text>
+      <Text>Hello wolrd 2</Text>
+      <Teste value="Olá" />
     </View>
   )
 }
