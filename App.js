66e46b9b93334a634afad4bb4cf29fc7f407e201
Andre gois <andre.s.gois3@gmail.com>
2021-11-13 12:25:12 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "./App.module";
+
+const App = () => {
+
+  return(
+    <View>
+      <Text>Hello wolrd</Text>
+    </View>
+  )
+}
+
+export default App;
